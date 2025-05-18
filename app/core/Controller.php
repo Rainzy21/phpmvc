f02809eb3819876bf838e8809214a1673e61ec85
epo33m <rivoldyvoldy@gmail.com>
2025-05-18 15:09:28 +0800

    revisi

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,14 +1,13 @@
 <?php
 // Base Controller class
 // This class can be extended by other controllers
-
-class Controller{
-    public function view($view, $data = []){
-        require_once '../app/views/' . $view . '.php';
-    }
-
-    public function model($model){
+class Controller {
+    // Untuk load model dan view
+    public function model($model) {
         require_once '../app/models/' . $model . '.php';
         return new $model;
+    }
+    public function view($view, $data = []) {
+        require_once '../app/views/' . $view . '.php';
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@
 
 require_once '../app/init.php';
 
-$app = new App;
+$app = new App();
